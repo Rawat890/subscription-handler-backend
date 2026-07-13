16453862caf6bc8a03c69fac1f1550cbe420193d
Rawat890 <rohanrawat7508@gmail.com>
2026-07-13 10:47:10 +0530

    chore: add regex utility functions for email, password, name, and phone validation

---
+++ src/utils/regex.js
@@ -0,0 +1,4 @@
+export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+export const nameRegex = /^[a-zA-Z\s]+$/
+export const phoneRegex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
