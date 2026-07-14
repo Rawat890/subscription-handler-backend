96d793da4c97822525eebeafa0186bca99d6661d
Rawat890 <rohanrawat7508@gmail.com>
2026-07-14 20:12:36 +0530

    chore: update MongoDB connection to use MONGO_URL and set DNS servers

--- src/database/db.js
+++ src/database/db.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose"
-import { MONGO_URI } from "../config/config.js";
+import { MONGO_URL } from "../config/config.js";
+import { promises as dns } from 'node:dns';
 
-const connectDB = async ()=>{
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+if (!MONGO_URL) {
+ throw new Error("MONGO_URL is not defined in environment variables");
+}
+
+const connectDB = async () => {
  try {
-  const connection = await mongoose.connect(MONGO_URI);
+  const connection = await mongoose.connect(MONGO_URL);
   console.log("Connected to mongo database -> ", connection.connection.db.databaseName);
  } catch (error) {
   console.log("Error connecting to mongo database -> ", error);
