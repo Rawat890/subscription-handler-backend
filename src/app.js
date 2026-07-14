bb2acba40aa2d316957c2189e6344104cf4e0856
Rawat890 <rohanrawat7508@gmail.com>
2026-07-14 20:12:09 +0530

    chore: reorganize app.js for improved structure and readability

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cors from 'cors';
-
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import errorMiddleware from './middlewares/error.middleware.js';
 const app = express();
 app.use(cors());
 app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+
+app.use(errorMiddleware);
 export default app;
