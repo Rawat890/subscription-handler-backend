20957127a7edb7d6bdb942c6c0b9619d96d8a4fa
Rawat890 <rohanrawat7508@gmail.com>
2026-07-15 11:13:05 +0530

    chore: enhance createSubscriptionController to trigger workflow and return workflowRunId

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,5 +1,7 @@
 
+import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js";
+import { SERVER_URL } from "../config/config.js";
 
 export const createSubscriptionController = async (req, res, next) => {
  console.log("req.user:", req.user);
@@ -10,9 +12,22 @@ export const createSubscriptionController = async (req, res, next) => {
    user: req.user._id
   });
 
+  const { workflowRunId } = await workflowClient.trigger({
+   url: `${SERVER_URL}/api/v1/workflows/subscriptions/reminder`,
+   body: {
+    subscriptionId: subscription.id
+   },
+   headers: {
+    "Content-Type": "application/json"
+   },
+   retries: 0
+  })
+
+  console.log("Workflow run id -", workflowRunId)
+
   res.status(200).json({
    success: true,
-   data: subscription,
+   data: { subscription, workflowRunId },
    message: "Subscription created successfully"
   })
  } catch (error) {
@@ -24,10 +39,11 @@ export const createSubscriptionController = async (req, res, next) => {
 
 export const getUserSubscriptionsController = async (req, res, next) => {
  try {
+  // check if the user is the same as the one in the token
   if (req.user.id !== req.params.id) {
    return res.status(401).json({
     success: false,
-    message: "Unauthorized to access this route"
+    message: "Unauthorized to access getUsersSubscription route"
    })
   }
 
