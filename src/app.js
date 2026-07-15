def734e708fc55b87caada8d7000eb1b279a7fc5
Rawat890 <rohanrawat7508@gmail.com>
2026-07-15 11:13:49 +0530

    chore: update app.js to include workflow routes and standardize endpoint structure

--- src/app.js
+++ src/app.js
@@ -5,14 +5,19 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import errorMiddleware from './middlewares/error.middleware.js';
+import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 app.use(errorMiddleware);
 export default app;
