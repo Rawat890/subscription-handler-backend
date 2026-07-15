b009ad33d69b66c496147c92c0de307ecaf7f63b
Rawat890 <rohanrawat7508@gmail.com>
2026-07-15 08:31:13 +0530

    chore: add logging for request user and body in createSubscriptionController

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,6 +2,8 @@
 import Subscription from "../models/subscription.model.js";
 
 export const createSubscriptionController = async (req, res, next) => {
+ console.log("req.user:", req.user);
+ console.log("req.body:", req.body);
  try {
   const subscription = await Subscription.create({
    ...req.body,
@@ -14,12 +16,29 @@ export const createSubscriptionController = async (req, res, next) => {
    message: "Subscription created successfully"
   })
  } catch (error) {
-  console.log("Subscription creation error: ",error);
+  console.log("Subscription creation error: ", error);
   next(error)
  }
 }
 
 
-export const getUserSubscriptionsController = async (req, res, next) =>{
- 
+export const getUserSubscriptionsController = async (req, res, next) => {
+ try {
+  if (req.user.id !== req.params.id) {
+   return res.status(401).json({
+    success: false,
+    message: "Unauthorized to access this route"
+   })
+  }
+
+  const subscriptions = await Subscription.find({ user: req.user._id });
+
+  res.status(200).json({
+   success: true,
+   data: subscriptions,
+   message: "Subscriptions fetched successfully"
+  })
+ } catch (error) {
+  next(error);
+ }
 }
